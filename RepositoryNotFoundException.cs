e141b8192bffb9a5565f035b4748d5c88ccd4061
thZero.com <development@thzero.com>
2018-08-13 07:25:10 -0500

    master

---
+++ RepositoryNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace thZero.Data.Repository
+{
+    [Serializable]
+    public class RepositoryNotFoundException : Exception
+    {
+        public RepositoryNotFoundException() : base() { }
+        public RepositoryNotFoundException(Type type) : base(type != null ? type.FullName : string.Empty) { }
+        public RepositoryNotFoundException(string message) : base(message) { }
+        public RepositoryNotFoundException(string message, Exception inner) : base(message, inner) { }
+#pragma warning disable CS0628 // New protected member declared in sealed class
+        protected RepositoryNotFoundException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+#pragma warning restore CS0628 // New protected member declared in sealed class
+    }
+}
